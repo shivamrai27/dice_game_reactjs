948850e5b2de1a5185d6506499236d6f9b6446a9
shivamrai27 <200shivamrai@gmail.com>
2023-10-11 01:32:22 +0530

    dice game

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import StartGame from './components/StartGame';
 import { useState } from 'react';
 import GamePlay from './components/GamePlay';
 
+
 function App() {
   const [isGameStarted, setIsGameStarted] = useState(false);
   const toggleGamePlay = () => {
--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const StartGame = () => {
+const StartGame = ({ toggle }) => {
     return (
         <Container>
 
             <div>
                 <img src='images/dices.png' alt='Dice Image' />
             </div>
-            <div>
+            <div className='content'>
                 <h1>Dice Game</h1>
-                <Button>Play Now</Button>
+                <Button onClick={toggle}>Play Now</Button>
             </div>
         </Container>
     )
@@ -18,10 +18,18 @@ const StartGame = () => {
 
 const Container = styled.div`
     max-width: 1180px;
+    height: 100vh;
     display: flex;
     margin:0 auto;
     align-items: center;
-    
+
+    .content{
+        h1{
+            font-size: 96px;
+            white-space: nowrap;
+        }
+    }
+
 `
 const Button = styled.button`
     color: white;
@@ -30,6 +38,20 @@ const Button = styled.button`
     border-radius: 5px;
     min-width: 220px;
     border: none;
+    font-size: 16px;
+    border: 1px solid transparent;
+    transition: 0.3s ease-in;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: ;
+    &:hover{
+        background-color: white;
+        color: black;
+        border: 1px solid black;
+        transition: 0.2s ease-in;
+        cursor: pointer;
+    }
 `
 
 
